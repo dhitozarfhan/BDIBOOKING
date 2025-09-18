96758b636d787e1b82329d1fac16eb0e8edad857
saaandbite <haris.sandi23@students.utdi.ac.id>
2025-09-18 16:01:39 +0700

    perintilan update filter

--- resources/views/filament/pages/archive-page.blade.php
+++ resources/views/filament/pages/archive-page.blade.php
@@ -534,7 +534,7 @@
                         
                         <!-- Date Range Filter -->
                         <div class="filter-group">
-                            <label class="filter-label">{{ __('Rentang Waktu') }}</label>
+                            <label class="filter-label">{{ __('Rentang Waktu Berkas') }}</label>
                             <div class="grid grid-cols-1 sm:grid-cols-2 gap-3">
                                 <div>
                                     <label class="block text-xs text-gray-500 mb-1">{{ __('Tanggal Mulai') }}</label>
@@ -564,18 +564,14 @@
                             id="cancelFilter"
                             class="btn btn-secondary"
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-                            </svg>
+                            
                             {{ __('Batal') }}
                         </button>
                         <button 
                             type="submit" 
                             class="btn btn-primary"
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-                            </svg>
+                            
                             {{ __('Terapkan Filter') }}
                         </button>
                     </div>
