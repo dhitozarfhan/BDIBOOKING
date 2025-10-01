f1ff58867a446f9d5dcc8ce6da22a611752f699f
Ahmad Dedad <projectdummy2025@gmail.com>
2025-10-01 12:48:18 +0700

    menambahkan AccountResource

---
+++ app/Filament/Resources/AccountResource.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AccountResource\Pages;
+use App\Models\Account;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class AccountResource extends Resource
+{
+    protected static ?string $model = Account::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationLabel = 'Akun Keuangan';
+
+    protected static ?string $pluralModelLabel = 'Akun Keuangan';
+
+    protected static ?int $navigationSort = 23;
+
+    public static function getNavigationGroup(): ?string
+    {
+        return __('Arsip');
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Grid::make([
+                    'default' => 1,
+                    'md' => 2,
+                ])
+                ->schema([
+                    Forms\Components\Section::make()
+                        ->columnSpan(2)
+                        ->schema([
+                            Forms\Components\TextInput::make('code')
+                                ->label(__('Code'))
+                                ->required()
+                                ->maxLength(255)
+                                ->unique(ignoreRecord: true),
+                            
+                            Forms\Components\TextInput::make('name')
+                                ->label(__('Name'))
+                                ->required()
+                                ->maxLength(255),
+                        ]),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('code')
+                    ->label(__('Code'))
+                    ->searchable()
+                    ->sortable(),
+                
+                Tables\Columns\TextColumn::make('name')
+                    ->label(__('Name'))
+                    ->searchable()
+                    ->sortable(),
+                
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label(__('Created At'))
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label(__('Updated At'))
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAccounts::route('/'),
+            'create' => Pages\CreateAccount::route('/create'),
+            'edit' => Pages\EditAccount::route('/{record}/edit'),
+        ];
+    }
+}
