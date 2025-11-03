57d112d5a87105e3de548e78e54bc63f0624387c
projectdummy2025 <projectdummy2025@gmail.com>
2025-11-03 13:58:08 +0000

    update untuk recaptcha dummy untuk gratifikasi sementara

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,8 +22,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-	if(env('REDIRECT_HTTPS')){
+        Validator::extend('recaptcha', function ($attribute, $value, $parameters, $validator) {
+            $response = Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
+                'secret' => env('NOCAPTCHA_SECRET'),
+                'response' => $value,
+            ]);
+
+            return $response->json('success');
+        });
+
+        if (env('REDIRECT_HTTPS')) {
             URL::forceScheme('https');
             request()->server->set('HTTPS', request()->header('X-Forwarded-Proto', 'https') == 'https' ? 'on' : 'off');
         }
