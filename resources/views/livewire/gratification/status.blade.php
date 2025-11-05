535dec32ce78ee688924e2c6355285b31be47871
projectdummy2025 <projectdummy2025@gmail.com>
2025-11-05 05:54:12 +0000

    style cek status gratification

--- resources/views/livewire/gratification/status.blade.php
+++ resources/views/livewire/gratification/status.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="p-4 sm:p-8 bg-base-100 shadow sm:rounded-lg">
+    <div class="p-4 sm:p-8 bg-base-100 shadow sm:rounded-lg mt-4 pb-10">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="text-sm breadcrumbs">
             <ul>
