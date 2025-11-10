9309ebb28bb9ffd2175f29470d1211e957cb3194
saaandbite <haris.sandi23@students.utdi.ac.id>
2025-11-10 12:57:44 +0700

    mempercantik pengaduan masyarakat part 2

--- resources/views/components/information-sidebar.blade.php
+++ resources/views/components/information-sidebar.blade.php
@@ -1,27 +1,27 @@
-<div class="bg-white dark:bg-gray-800 rounded-lg shadow p-6">
-    <div class="mb-6 pb-6 border-b border-gray-200 dark:border-gray-700">
-        <h2 class="text-xl font-bold text-gray-900 dark:text-white mb-4">{{ __('information.contact') }}</h2>
-        <ul class="space-y-2 text-gray-700 dark:text-gray-300">
+<div>
+    <div class="mb-6 pb-6 border-b border-base-300">
+        <h2 class="text-xl font-bold text-base-content mb-4">{{ __('information.contact') }}</h2>
+        <ul class="space-y-2 text-base-content/80">
             <li class="flex items-center">
-                <i class="fas fa-phone-alt w-5 h-5 mr-2 text-blue-500"></i>
+                <i class="fas fa-phone-alt w-5 h-5 mr-2 text-primary"></i>
                 <span><strong>{{ __('information.phone') }}:</strong> {{ __('information._phone') }}</span>
             </li>
             <li class="flex items-center">
-                <i class="fas fa-fax w-5 h-5 mr-2 text-blue-500"></i>
+                <i class="fas fa-fax w-5 h-5 mr-2 text-primary"></i>
                 <span><strong>{{ __('information.fax') }}:</strong> {{ __('information._fax') }}</span>
             </li>
             <li class="flex items-start">
-                <i class="fas fa-map-marker-alt w-5 h-5 mr-2 text-blue-500 mt-1"></i>
+                <i class="fas fa-map-marker-alt w-5 h-5 mr-2 text-primary mt-1"></i>
                 <span><strong>{{ __('information.address') }}:</strong> {{ __('information._address') }}</span>
             </li>
             <li class="flex items-center">
-                <i class="fas fa-envelope w-5 h-5 mr-2 text-blue-500"></i>
-                <a href="mailto:bdiyogyakarta@kemenperin.go.id" class="text-blue-600 hover:underline">{{ __('information._email') }}</a>
+                <i class="fas fa-envelope w-5 h-5 mr-2 text-primary"></i>
+                <a href="mailto:bdiyogyakarta@kemenperin.go.id" class="link link-primary">{{ __('information._email') }}</a>
             </li>
         </ul>
     </div>
-    <div class="mb-6 pb-6 border-b border-gray-200 dark:border-gray-700">
-        <h2 class="text-xl font-bold text-gray-900 dark:text-white mb-4">{{ __('information.maps') }}</h2>
+    <div class="mb-6 pb-6 border-b border-base-300">
+        <h2 class="text-xl font-bold text-base-content mb-4">{{ __('information.maps') }}</h2>
         <div class="rounded-lg overflow-hidden">
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7905.548842741774!2d110.403105!3d-7.813687!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x310b8a2efcab039a!2sBALAI+DIKLAT+INDUSTRI!5e0!3m2!1sen!2sus!4v1496382397073"
@@ -30,10 +30,10 @@
         </div>
     </div>
     <div>
-        <h2 class="text-xl font-bold text-gray-900 dark:text-white mb-4">{{ __('information.facebook') }}</h2>
+        <h2 class="text-xl font-bold text-base-content mb-4">{{ __('information.facebook') }}</h2>
         <div class="flex items-center">
-            <i class="fab fa-facebook-square w-5 h-5 mr-2 text-blue-500"></i>
-            <a href="https://www.facebook.com/bdiyogyakarta" class="text-blue-600 hover:underline">bdiyogyakarta</a>
+            <i class="fab fa-facebook-square w-5 h-5 mr-2 text-primary"></i>
+            <a href="https://www.facebook.com/bdiyogyakarta" class="link link-primary">bdiyogyakarta</a>
         </div>
     </div>
 </div>
