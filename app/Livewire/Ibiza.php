3d0ca41a38a9ed70cb05aebfa42d261ba0cb1155
projectdummy2025 <projectdummy2025@gmail.com>
2025-11-11 05:41:52 +0000

    add ibiza (liveware)

---
+++ app/Livewire/Ibiza.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Ibiza extends Component
+{
+    public function render()
+    {
+        return view('livewire.ibiza')
+            ->title('Ibiza - Inkubator Bisnis'); // Add a title for the page
+    }
+}
---
+++ resources/views/livewire/ibiza.blade.php
@@ -0,0 +1,116 @@
+<section class="bg-gray-100">
+    <div class="container mx-auto px-4 mt-6 mb-16">
+        <div class="card bg-white p-6 mb-5 shadow-lg rounded-lg">
+            <div class="mb-5">
+                <h2 class="text-4xl font-bold text-gray-800">Ibiza = Inkubator Bisnis</h2>
+                <h4 class="text-2xl font-semibold text-gray-800">Inkubator Bisnis Pazti Bisa</h4>
+            </div>
+            <div class="border-t border-b py-4">
+                <p class="leading-relaxed font-semibold text-lg">
+                    Balai Diklat Industri Yogyakarta mengambil semangat bahwa Inkubator Bisnis Pazti Bisa (Ibiza)
+                    agar para tenant
+                    memiliki jiwa optimis dalam mengembangkan usaha barunya. Tenant yang menjadi fokus Ibiza adalah
+                    wirausaha di bidang
+                    industri <b class="text-green-500">plastik</b>, <b class="text-blue-500">kerajinan kulit</b> dan
+                    <b class="text-red-500">produk kulit</b>. </br>
+                    <a href="https://ibiza.id"
+                        class="btn mt-2 bg-yellow-500 text-black font-semibold py-2 px-4 items-center rounded-lg shadow-md hover:bg-yellow-600">
+                        <i class="fas fa-suitcase mr-2"></i> Daftar Sekarang
+                    </a>
+                </p>
+            </div>
+            <div class="py-4">
+                <p class="text-gray-600">
+                    Program pengembangan wirausaha nasional telah menjadi salah satu program utama pemerintah
+                    sebagaimana diamanatkan
+                    dalam <b>Perpres No. 27 Tahun 2013</b> dalam penyelenggaraan kegiatan inkubator bisnis.
+                    <b>Undang-undang No. 3 Tahun 2014</b>
+                    tentang Perindustrian menyebutkan bahwa pembinaan sumber daya manusia industri juga mencakup
+                    pembinaan wirausaha
+                    industri yang mana tujuannya adalah untuk menciptakan wirausaha yang berkarakter dan bermental
+                    kewirausahaan serta
+                    berkompetensi di bidang usahanya. Pembinaan wirausaha industri dilakukan melalui pelatihan,
+                    kemitraan dan inkubator
+                    wirausaha atau inkubator industri.
+                    <br>
+                    Pada tahap perkembangan bisnis, yang paling banyak menghadapi risiko kegagalan yaitu bisnis pada
+                    tahap pemula
+                    (<em>start up</em>). Oleh karena itu, di negara-negara maju seperti Amerika dan negara-negara
+                    Eropa, mereka membentuk system
+                    atau infrastruktur agar resiko dapat diperkecil, dengan membuat lembaga yang namanya
+                    <b><em>incubator</em></b>, yaitu suatu fasilitas
+                    dan aktivitas yang diberikan kepada pengusaha pemula yang biasa disebut penyewa (tenant) dalam
+                    bentuk fisik, misalnya
+                    tempat memulai mengembangkan usaha, fasilitas produksi, pelayanan pelatihan, akses kepada
+                    teknologi, tenaga kerja, modal
+                    dan pasar. Di sini pengusaha saling berinteraksi sehingga dapat saling tolong menolong, berbagi
+                    pengalaman, dan sebagainya,
+                    yang menyebabkan inkubator menjadi tempat kondusif untuk tumbuhnya usaha baru.
+                </p>
+            </div>
+        </div>
+        <h2 class="text-center text-3xl font-bold my-12">Layanan inkubasi yang didapatkan oleh para tenant</h2>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <!-- Card 1 -->
+            <div class="bg-white p-6 rounded-lg shadow-md">
+                <span class="fa-stack fa-2x">
+                    <i class="fas fa-square fa-stack-2x text-green-500"></i>
+                    <i class="fas fa-hammer fa-stack-1x fa-inverse"></i>
+                </span>
+                <h2 class="text-xl font-bold mb-2">Pengembangan Produk</h2>
+                <ul class="list-none mb-4">
+                    <li class="flex items-center text-green-500 mb-2">
+                        <i class="fas fa-check mr-2"></i> Product Development
+                    </li>
+                    <li class="flex items-center text-green-500 mb-2">
+                        <i class="fas fa-check mr-2"></i> Product Simulation
+                    </li>
+                    <li class="flex items-center text-green-500 mb-2">
+                        <i class="fas fa-check mr-2"></i> Evaluasi Bulanan dan Akhir
+                    </li>
+                </ul>
+                <p class="text-gray-600">Bimbingan teknis, bimbingan manjerial, konseling tim, penjadwalan project,
+                    penentuan target capaian mingguan-bulanan, evaluasi capaian mingguan-bulanan, evaluasi akhir
+                    capaian Busines Model Canvas</p>
+            </div>
+            <!-- Card 2 -->
+            <div class="bg-white p-6 rounded-lg shadow-md">
+                <span class="fa-stack fa-2x">
+                    <i class="fas fa-square fa-stack-2x text-blue-500"></i>
+                    <i class="fas fa-shopping-bag fa-stack-1x fa-inverse"></i>
+                </span>
+                <h2 class="text-xl font-bold mb-2">Pengembangan Pasar</h2>
+                <ul class="list-none mb-4">
+                    <li class="flex items-center text-blue-500 mb-2">
+                        <i class="fas fa-check mr-2"></i> Konsultasi Bisnis
+                    </li>
+                    <li class="flex items-center text-blue-500 mb-2">
+                        <i class="fas fa-check mr-2"></i> Business Matching
+                    </li>
+                    <li class="flex items-center text-blue-500 mb-2">
+                        <i class="fas fa-check mr-2"></i> Evaluasi
+                    </li>
+                </ul>
+                <p class="text-gray-600">Bimbingan teknis, bimbingan manjerial, konseling tim, penjadwalan project,
+                    fasilitasi pameran/temu pelanggan, evaluasi akhir capaian Busines Model Canvas</p>
+            </div>
+            <!-- Card 3 -->
+            <div class="bg-white p-6 rounded-lg shadow-md">
+                <span class="fa-stack fa-2x">
+                    <i class="fas fa-square fa-stack-2x text-red-500"></i>
+                    <i class="fas fa-graduation-cap fa-stack-1x fa-inverse"></i>
+                </span>
+                <h2 class="text-xl font-bold mb-2">Start Up Graduate Program</h2>
+                <ul class="list-none mb-4">
+                    <li class="flex items-center text-red-500 mb-2">
+                        <i class="fas fa-check mr-2"></i> Memantau perkembangan pasca program inkubasi
+                    </li>
+                    <li class="flex items-center text-red-500 mb-2">
+                        <i class="fas fa-check mr-2"></i> Pendampingan serta pemantauan tetap dilakukan dengan
+                        intensitas yang lebih rendah
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
