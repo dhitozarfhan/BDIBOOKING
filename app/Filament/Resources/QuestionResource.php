b1d5b0ffcedfa075e918b221e445f4142bd9b6dd
saaandbite <haris.sandi23@students.utdi.ac.id>
2025-11-13 12:49:31 +0700

    tambah pengaduan masyarakat di filament

---
+++ app/Filament/Resources/QuestionResource.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\QuestionResource\Pages;
+use App\Models\Question;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class QuestionResource extends Resource
+{
+    protected static ?string $model = Question::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-question-mark-circle';
+
+    public static function getNavigationGroup(): ?string
+    {
+        return __('Report');
+    }
+
+    public static function getModelLabel(): string
+    {
+        return __('Public Complaint');
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('subject')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\RichEditor::make('content')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('mobile')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('subject')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('mobile')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime('d/m/Y H:i')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListQuestions::route('/'),
+            'view' => Pages\ViewQuestion::route('/{record}'),
+        ];
+    }
+}
