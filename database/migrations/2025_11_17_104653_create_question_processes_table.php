4f6945c7affeea8c966cf66ee2e6292cb2ca286d
saaandbite <haris.sandi23@students.utdi.ac.id>
2025-11-17 11:00:19 +0700

    edit pengaduan masyarakat

---
+++ database/migrations/2025_11_17_104653_create_question_processes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('question_processes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('question_id')->constrained('questions')->onDelete('cascade');
+            $table->foreignId('response_status_id')->default(1)->constrained('response_statuses');
+            $table->text('answer')->nullable(); // Answer/response to the report
+            $table->string('answer_attachment')->nullable(); // Answer attachment similar to attachment in gratifications table
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('question_processes');
+    }
+};
