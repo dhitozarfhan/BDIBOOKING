af88165eca62b5a0626b00e7a34ec65382858cf1
Ahmad Dedad <projectdummy2025@gmail.com>
2025-12-22 14:14:31 +0700

    hapus lampiran pada dispositionresource

--- app/Filament/Resources/DispositionResource.php
+++ app/Filament/Resources/DispositionResource.php
@@ -118,21 +118,6 @@ class DispositionResource extends Resource
                     ->label(__('Created At'))
                     ->dateTime()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('answer_attachment')
-                    ->label(__('Attachment'))
-                    ->formatStateUsing(function ($state) {
-                        if (empty($state)) {
-                            return '-';
-                        }
-                        $url = route('download', ['path' => $state]);
-                        return '<a href="' . $url . '" target="_blank" class="filament-link inline-flex items-center gap-1">
-                                    <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                      <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
-                                    </svg>
-                                    <span>Download</span>
-                                </a>';
-                    })
-                    ->html(),
             ])
             ->filters([
                 //
